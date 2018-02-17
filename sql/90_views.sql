7f345de25e44fa6704afb583019149e441a7a14e
Evan Carroll <me@evancarroll.com>
2018-02-17 13:58:44 -0600

    added a view for expanded display using index and index on demographics_code

--- sql/90_views.sql
+++ sql/90_views.sql
@@ -10,7 +10,18 @@ AS
 	JOIN census.subcategory AS s
 		USING (category_code);
 
-CREATE VIEW census.view_alldata
+CREATE VIEW census.view_alldata_simple
 AS
 	SELECT (census.parse_series_id(series_id)).*, year, value, footnote_codes
 	FROM census.alldata;
+
+CREATE VIEW census.view_alldata_expanded
+AS
+	SELECT cad.year, cad.value, ci.item_text, cd.demographics_text, cc.characteristics_text
+	FROM census.alldata AS cad
+	JOIN census.item AS ci
+		ON (census.parse_series_id(series_id)).item_code = ci.item_code
+	JOIN census.demographics AS cd
+		ON (census.parse_series_id(series_id)).demographics_code = cd.demographics_code
+	JOIN census.characteristics AS cc
+		 ON (census.parse_series_id(series_id)).characteristics_code = cc.characteristics_code;
