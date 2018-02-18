e01a1a0f2d537e05c136603e81f57b0f8bcc7707
Evan Carroll <me@evancarroll.com>
2018-02-17 19:40:07 -0600

    ids
    * fixed the problem with item_code (varchar(8) not char(6)).
    * all the other ids fixed right to be more specific now the extended
    view works though slow.

--- sql/06_funcs.sql
+++ sql/06_funcs.sql
@@ -1,14 +1,8 @@
+-- regexp_matches('CXUALCBEVGLB0101M'::text, '^(.{2})(.)(.*?)(.{4})(.{2})(.)$');
 CREATE FUNCTION census.parse_series_id(series_id text)
 RETURNS census.series_id AS $$
-	SELECT
-		(
-			substring(series_id FROM 1 FOR 2), -- AS survey_abbreviation,
-			substring(series_id FROM 3 FOR 1), -- AS seasonal,
-			substring(series_id FROM 4 FOR 6), -- AS item_code,
-			substring(series_id FROM 10 FOR 4), -- AS demographics_code,
-			substring(series_id FROM 14 FOR 2), -- AS characteristics_code,
-			substring(series_id FROM 16 FOR 1 ) -- AS process_code
-		)::census.series_id
-	FROM ( VALUES (series_id) ) AS t(series_id)
+	SELECT (a[1],a[2],a[3],a[4],a[5],a[6])::census.series_id
+	FROM regexp_matches(series_id, '^(.{2})(.)(.{4,})(.{4})(.{2})(.)$')
+		AS t(a);
 $$ LANGUAGE sql
 IMMUTABLE;
